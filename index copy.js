c55d97df5ab7ea6dcc8c0effc4eb02cb9a583918
Jordan <jmwilliams94@icloud.com>
2022-03-03 17:24:28 +1100

    Completed Lab

---
+++ index copy.js
@@ -0,0 +1,18 @@
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return ("This one is on me!")
+  }   
+  else if (someValue >= 2000) {
+    return ("I will gladly take your thirty bucks.")
+  }
+  else if (someValue > 2500) 
+    return ("No can do.")
+  }
+
+function ternaryCheckCity(){
+  // Write your code here!
+}
+
+function switchOnCharmFromTip(){
+  // Write your code here!
+}
